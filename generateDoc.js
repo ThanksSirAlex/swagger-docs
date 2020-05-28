1ebfe08ca00454de69f7476b19c47eeede441e10
ThanksSirAlex <thankssiralex@163.com>
2020-05-28 16:19:14 +0800

    change route

---
+++ generateDoc.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const readlineSync = require('readline-sync');
+
+let projectName = readlineSync.question("Please input your project name. (use '_' if needed): ", {defaultInput: 'project_name'});
+let projectDir = path.join(__dirname, "docs", projectName);
+
+while (fs.existsSync(projectDir)) {
+    projectName = readlineSync.question("Project name already exist. Chose another one. (enter n to exit): ", {defaultInput: 'project_name'});
+    if (projectName === 'n') {
+        console.log(Exiting)
+        process.exit();
+    }
+    projectDir = path.join(__dirname, "docs", projectName);
+}
+
+let swaggerVersion = readlineSync.question("Choese the version of swagger to use. (2/3): ", {defaultInput: 3})
+
+while (!['2', '3'].includes(swaggerVersion)) {
+    console.log("Invalid version should be either 2 or 3");
+    swaggerVersion = readlineSync.question("Invalid version should be either 2 or 3, try again. (enter n to exit): ", {defaultInput: 3})
+    if (swaggerVersion === 'n') {
+        console.log(Exiting)
+        process.exit();
+    }
+}
+
+swaggerVersion = parseInt(swaggerVersion, 10)
+
+metaJson = {}
+if (swaggerVersion === 2) {
+    metaJson['swagger'] = "2.0"
+    fs.mkdirSync(projectDir);
+    fs.writeFileSync(`${projectDir}/metadata.yml`, yaml.safeDump(metaJson));
+    fs.writeFileSync(`${projectDir}/tags.yml`, yaml.safeDump({tags: {}}));
+    fs.writeFileSync(`${projectDir}/securityDefinitions.yml`, yaml.safeDump({securityDefinitions: {}}));
+    fs.mkdirSync(`${projectDir}/paths`);
+    fs.mkdirSync(`${projectDir}/definitions`);
+} else {
+    metaJson['openapi'] = "3.0.0"
+    fs.mkdirSync(projectDir);
+    fs.writeFileSync(`${projectDir}/metadata.yml`, yaml.safeDump(metaJson));
+    fs.writeFileSync(`${projectDir}/tags.yml`, yaml.safeDump({tags: {}}));
+    fs.mkdirSync(`${projectDir}/paths`);
+    fs.writeFileSync(`${projectDir}/paths/.keep`, '');
+    fs.mkdirSync(`${projectDir}/components`);
+    fs.writeFileSync(`${projectDir}/components/schemas.yml`, yaml.safeDump({schemas: {}}));
+    fs.writeFileSync(`${projectDir}/components/parameters.yml`, yaml.safeDump({parameters: {}}));
+    fs.writeFileSync(`${projectDir}/components/securitySchemes.yml`, yaml.safeDump({securitySchemes: {}}));
+    fs.writeFileSync(`${projectDir}/components/requestBodies.yml`, yaml.safeDump({requestBodies: {}}));
+    fs.writeFileSync(`${projectDir}/components/responses.yml`, yaml.safeDump({responses: {}}));
+    fs.writeFileSync(`${projectDir}/components/headers.yml`, yaml.safeDump({headers: {}}));
+}
+
+Console.log('Done')
--- src/routes.js
+++ src/routes.js
@@ -1,48 +1,12 @@
 import React from 'react';
 import SwaggerUI from "./components/swagger";
-import i18n from './i18n'
 
 const dashboardRoutes = [
-  {
-    path: "/swagger",
-    name: "Swagger",
-    component: () => <SwaggerUI />
-  },
-  {
-    path: "/user",
-    name: "User Profile",
-    component: () => <SwaggerUI  />
-  },
-  {
-    path: "/table",
-    name: "Table List",
-    component: () => <SwaggerUI  />
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    component: () => <SwaggerUI  />
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    component: () => <SwaggerUI  />
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    component: () => <SwaggerUI  />
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    component: () => <SwaggerUI  />
-  },
-  {
-    path: "/test",
-    name: i18n.t('test'),
-    component: () => <SwaggerUI url = {process.env.PUBLIC_URL + '/swagger/test.json'} />
-  }
+    {
+        path: "/test",
+        name: i18n.t('test'),
+        component: () => <SwaggerUI url={process.env.PUBLIC_URL + '/swagger/test.json'}/>
+    }
 ];
 
 export default dashboardRoutes;
